b1467f22cf35583e3c746ecf0f0ee99c7e3326cd
bijekarv <bijekarv@gmail.com>
2025-04-22 19:27:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-091dccf4e2d272bae"
-instance_type = "t2.micro"
-key_name = "kp"
-vpc_security_group_ids = ["sg-094906e3040fd4199"]
-tags = {
-Name = var.instance_names[count.index]
-}
+  count = 4
+  ami = "ami-091dccf4e2d272bae"
+  instance_type = "t2.micro"
+  key_name = "kp"
+  vpc_security_group_ids = ["sg-094906e3040fd4199"]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
 }
 
 variable "instance_names" {
-default = ["jenkins" , "tomcat-1" , "tomcat-2" , "Monitoring"]
+  default = ["jenkins" , "tomcat-1" , "tomcat-2" , "Monitoring"]
 }
